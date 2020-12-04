92b75ee6287c1d97245c6cc329dfd7167f680b3e
David <David_cote_09@hotmail.com>
2020-12-03 20:08:55 -0500

    demander_ouinon / bouton_quitter

--- Fichiers-TP4/interface_partie.py
+++ Fichiers-TP4/interface_partie.py
@@ -2,18 +2,23 @@
 TODO: Ce fichier présente une ébauche d'interface pour le TP4. Vous pouvez le modifier à souhait.
 N'oubliez pas de commenter le code!
 """
+
 from tkinter import Tk, Frame, Button, messagebox
 from tableau import Tableau
 from bouton_case import BoutonCase
 
 
+
 class InterfacePartie(Tk):
     def __init__(self):
         super().__init__()
+        
 
         # Nom de la fenêtre.
         self.title("Démineur")
         self.resizable(0,0)
+
+        
 
         self.tableau_mines = Tableau()
 
@@ -23,11 +28,7 @@ class InterfacePartie(Tk):
         bouton_nouvelle_partie = Button(bouton_frame, text='Nouvelle partie', command=self.nouvelle_partie)
         bouton_nouvelle_partie.grid(row=0, column=0)
 
-        #J'ai essayé le yexnocancel du site https://docs.python.org/3/library/tkinter.messagebox.html#module-tkinter.messagebox
-        bouton_quitter = Tk.messagebox.askyernocancel(Bouton_frame, title = sexybox, message = coucou, **options)
-        
-        #C'est la ligne d'origine sauf la partie command qui est changé pour appoeler ma fonction plus bas
-        #bouton_quitter = Button(bouton_frame, text="Quitter", command=self.bouton_quitter2) 
+        bouton_quitter = Button(bouton_frame, text="Quitter", command=self.demander_ouinon)
         bouton_quitter.grid(row=0, column=1)
 
         self.cadre = Frame(self)
@@ -41,6 +42,7 @@ class InterfacePartie(Tk):
                 bouton.grid(row=i, column=j)
                 bouton.bind('<Button-1>', self.devoiler_case)
                 self.dictionnaire_boutons[(i+1, j+1)] = bouton
+      
 
     def devoiler_case(self, event):
         bouton = event.widget
@@ -56,27 +58,21 @@ class InterfacePartie(Tk):
         for bouton in self.dictionnaire_boutons.values():
             bouton['text'] = " "
 
-
-    """ def bouton_quitter2(self):
-        Auteur: David
+    def demander_ouinon(self):
+        """Auteur: David
         
         Note:
-            Fonction pour définir le bouton quitter
+            Demande à l'utilisateur s'il veut vraiment quitter le jeu
 
         Args:
             Inconnue
 
         Returns:
             None
-        
-        #bouton_quitter = Button(bouton_frame, text="Quitter", command=self.bouton_quitter2)
-        #bouton_quitter2.grid(row=0, column=1)
-
-        MsgBox = Tk.messagebox.askquestion ("Quittez l'application","VOulez-vous vraiment quitter le jeu super trop cool wow",icon = "warning")
-        if MsgBox == "yes":
-            root.destroy()
-        else:
-            Tk.messagebox.showinfo("Annuler","Bubye") """
+        """
+        question = messagebox.askyesno(title = "Quitter", message = "Voulez-vous vraiment quitter ? Genre tes sur de ta shot ?")
+        if question == True:
+            self.destroy()
            
 
 
