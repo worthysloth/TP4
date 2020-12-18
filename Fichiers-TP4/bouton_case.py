3a91ff288fb68285594e033d42f154790d5cc297
David <David_cote_09@hotmail.com>
2020-12-18 12:30:39 -0500

    Revert "Merge branch 'main' into David-2"
    This reverts commit 627e2fae31f68f25e0ea869283919aa1537bb094.

--- Fichiers-TP4/bouton_case.py
+++ Fichiers-TP4/bouton_case.py
@@ -12,14 +12,16 @@ class BoutonCase(Button):
         self.rangee_x = rangee_x
         self.colonne_y = colonne_y
         self.image = PhotoImage(file = self.chemin_image)
-        self.drapeau = False
+        self.relief = "raised"
+        self.red_flag = False
         super().__init__(
         parent,
         image=self.image, 
         padx=1, 
         pady=3, 
         height=24, 
-        width=24
+        width=24, 
+        relief=self.relief
         )
 
     def reinitialiser_image(self):
